9bd02a4c4e9f0e7e5956294df43d2970f6593c70
Luiz <luizrafael.apneto@gmail.com>
2024-09-23 18:42:59 -0300

    b

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
+
     public static void sayHello(){
         System.out.println("Hello world!");
     }
 
+    public static void sayHelloManyTimes(int times){
+        for (int i = 0; i < times; i++){
+            sayHello();
+        }
+    }
+
     public static void main(String[] args) {
-        sayHello();
+        sayHelloManyTimes(6);
     }
 }   
